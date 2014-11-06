8f46c20e66465f3b5e3736a7b56cfcbd903f5599
Jason Reading <jason.reading@gmail.com>
2014-11-06 21:47:42 +0000

    Content updates

--- index.php
+++ index.php
@@ -20,60 +20,68 @@
 <main>
     <article itemscope itemtype="http://data-vocabulary.org/Person">
         <div class="feature">
-            <h1>Hi, I’m <span itemprop="name">Jason Reading</span> and I am a <span itemprop="title">Web Developer</span>.</h1>
+            <div class="feature__container">
+                <div class="feature__cell"><h1>Hi, I’m <span itemprop="name">Jason Reading</span> and I am a <span
+                            itemprop="title">Web Developer</span>.</h1>
 
-            <p itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address" class="intro">I work at <a
-                    href="http://weebie.co.uk/" target="_blank" itemprop="affiliation">Weebie Ltd</a> in <span
-                    itemprop="locality">Brentwood</span>.</p>
+                    <p itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address" class="intro">I work
+                        at <a
+                            href="http://weebie.co.uk/" target="_blank" itemprop="affiliation">Weebie</a>, a digital
+                        studio in <span
+                            itemprop="locality">Brentwood</span>.</p></div>
+            </div>
         </div>
 
-        <div class="main">
-            <p>I use PHP with frameworks to create websites and web-applications.</p>
-
-            <p>I’m a full-stack developer, dealing with server infrastructure to back and front-end code. I can provision
-                environments and manage servers and deployments. I utilize <em>PHP</em> with frameworks like
-                <em>Symfony</em> to craft and
-                structure the web. Then, using <em>HTML5</em>, <em>CSS</em> (with <em>SASS</em>) and <em>Javascript</em>
-                (with
-                <em>jQuery</em> or <em>Angularjs</em>) on the
-                front-end to bring the websites to life and lift them off the page.</p>
-
-            <p>I’ve worked on a range of projects from simple one-pager websites for local businesses, creating experiences
-                using <em>WordPress</em> by creating templates and plugins, and simple CRUD applications to complex
-                web-applications
-                using <em>Symfony</em>.</p>
-
-            <p>To see some examples of my work, head to the <a href="http://weebie.co.uk/projects" target="_blank">Weebie
-                    portfolio page</a> or my
-                <a href="http://github.com/jasonreading" target="_blank">Github account page</a>.</p>
-        </div>
-        <div class="side">
-
-            <aside>
-                <h2>Weebie Ltd</h2>
-
-                <p>Weebie are a web design and development company in Brentwood.</p>
-
-                <p><a href="http://weebie.co.uk" target="_blank">http://weebie.co.uk/</a></p>
-
-                <p>
-                    Leigh House,<br/>
-                    Weald Road, <br/>
-                    Brentwood, <br/>
-                    Essex, CM14 4SX
+        <div class="container">
+            <div class="main">
+                <p>I use <em>PHP, MySQL, HTML, CSS</em> and <em>Javascript</em> to forge websites and tools for local
+                    businesses.
                 </p>
-            </aside>
-            <aside>
-                <h2>Web Design</h2>
-                <p>I work closely with <a href="http://heygreg.co.uk" target="_blank" rel="friend">Greg Owen</a>, check him out!</p>
-            </aside>
+
+                <p>I’m a full-stack PHP developer. I'm not afraid to get my hands dirty with anything from
+                    server provisioning, database design &amp; configuration on the back-end &mdash; to SCSS and
+                    Javascript on the front-end. &hellip;and everything in-between. </p>
+
+                <p>I've made a range of websites on top of technologies <em>Drupal</em>, <em>WordPress</em> and <em>Silex</em>.
+                </p>
+
+                <p>I utilize frameworks like <em>Symfony</em> to craft websites and tools with an
+                    <em><abbr title="Object Oriented Programming">OOP</abbr></em> approach. I love to really sink
+                    my teeth into creating full-featured web applications using <em>Symfony</em>, from
+                    <em><abbr title="Create Read Update and Delete">CRUD</abbr></em> to
+                    <em><abbr title="Representational state transfer">REST</abbr></em> based applications.</p>
+
+                <p>To see some examples of my work, head to the <a href="http://weebie.co.uk/projects" target="_blank">Weebie
+                        portfolio page</a> or my
+                    <a href="http://github.com/jasonreading" target="_blank">Github account page</a>.</p>
+            </div>
+            <div class="side">
+
+                <aside>
+                    <h2>Weebie Ltd</h2>
+
+                    <p>Weebie are a web design and development company in Brentwood.</p>
+
+                    <p><a href="http://weebie.co.uk" target="_blank">http://weebie.co.uk/</a></p>
+                </aside>
+                <aside>
+                    <h2>Web Design</h2>
+
+                    <p>I work closely with <a href="http://heygreg.co.uk" target="_blank" rel="friend">Greg Owen</a>,
+                        check him out!</p>
+                </aside>
+            </div>
         </div>
+
 
 
     </article>
 </main>
 
-<footer>&copy; Jason Reading <?php date_default_timezone_set('Europe/London'); echo date('Y'); ?></footer>
+<footer class="footer">
+    <div class="footer__container"><p>&copy; Jason Reading <?php date_default_timezone_set('Europe/London');
+            echo date('Y'); ?></p></div>
+</footer>
 <?php include '_analytics.php'; ?>
 </body>
 </html>
